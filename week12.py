e40712b0b9fcab0b3cfd750472fefe8b550b15d9
maxim <MRyklis@co.cuny.adlan>
2017-04-19 15:40:44 -0400

    update

--- week12.py
+++ week12.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from itertools import izip
 
 url = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y?order=by-date&offset=40&apikey=cca7ccbe60f842838bec1a4e5c7b8492'
 
@@ -7,6 +8,12 @@ response = requests.get(url)
 
 data = json.loads(response.text)
 
+new_dict = {}
+i = 0
+for key, value in data['results'][i].items():
+    new_dict
+    i += 1
 
-for key in data:
-    print key
+print new_dict
+
+
