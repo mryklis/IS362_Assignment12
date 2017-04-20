7ff2d9d56c448d8991ee0155ff7a34ed33f1bde1
maxim <MRyklis@co.cuny.adlan>
2017-04-20 14:09:56 -0400

    update

--- week12.py
+++ week12.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from itertools import izip
+import pandas as pd
 
 url = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y?order=by-date&offset=40&apikey=cca7ccbe60f842838bec1a4e5c7b8492'
 
@@ -8,12 +8,24 @@ response = requests.get(url)
 
 data = json.loads(response.text)
 
-new_dict = {}
-i = 0
-for key, value in data['results'][i].items():
-    new_dict
-    i += 1
+df = pd.DataFrame(data['results'])
 
-print new_dict
+print df
+
+# df2 = []
+#
+# i = 0
+# for values in df['multimedia']:
+#
+#     df2 = pd.DataFrame(values, index=[i])
+#     df3 = pd.DataFrame().append(df2)
+#     print df3
+#     i += 1
+#
+# # df4 = pd.DataFrame(df3)
+#
+# print df3
+
+
 
 
